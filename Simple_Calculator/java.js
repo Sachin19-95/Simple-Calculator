9a3fdc1c769e4cf42852de96edcb5497fb9229b8
Sachin19-95 <iamsachin.rawat1995@gmail.com>
2024-08-07 16:48:16 +0530

    Simple Calculator

---
+++ Simple_Calculator/java.js
@@ -0,0 +1,15 @@
+function input(num) {
+    var result = document.getElementById("result");
+    result.value += num;
+}
+
+function calc() {
+    var result = document.getElementById("result");
+    var output = eval(result.value);
+    result.value = output;
+}
+
+function cancel() {
+    var result = document.getElementById('result');
+    result.value = '';
+}
